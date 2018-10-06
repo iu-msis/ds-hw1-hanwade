a03cb492f72643791c41fa87f4716c01f7cb123f
hanwade <hanwade@indiana.edu>
2018-10-06 18:09:35 -0400

    added vue capabilities to change rider profiles

--- public/js/riderProfile.app.js
+++ public/js/riderProfile.app.js
@@ -1,22 +1,63 @@
 var riderProfileApp = new Vue({
   el: '#riderProfileVue',
   data: {
-    "name" : "Frederick Ruiz",
-    "country" : "Spain",
-    "birthdate" : "1977-08-07",
-    "email" : "Frederick.Ruiz@example.com"
-  },
+    "results":[
+        {
+          "gender":"",
+          "name":
+            {
+              "title":"",
+              "first":"",
+              "last":""
+            },
+          "location":
+            {
+              "street":"",
+              "city":"",
+              "state":"",
+              "postcode":null
+              },
+          "email":"",
+          "dob":
+            {
+              "date":"",
+              "age": null
+            },
+          "phone":"",
+          "cell":"",
+          "picture":
+            {
+              "large":"",
+              "medium":"",
+              "thumbnail":""
+            }
+        }
+      ],
+      },
   computed: {
     current_age: function() {
-      return moment().diff(this.birthdate, 'years');
+      return moment().diff(this.results.results[0].dob.date, 'years');
     },
     pretty_birthdate: function() {
-      return this.pretty_date(this.birthdate);
+      return this.pretty_date(this.results.results[0].dob.date);
     }
   },
   methods: {
     pretty_date: function (d) {
       return moment(d).format('l');
-    }
+    },
+    fetchRider() {
+      fetch( 'https://randomuser.me/api' )
+      .then( (response) => response.json() )
+      .then( (json) => {riderProfileApp.results = json})
+      .catch( function(err) {
+        console.log('RIDER FETCH ERROR:')
+        console.log(err)
+      })
+    },
+  },
+  created() {
+    this.fetchRider()
   }
+
 });
